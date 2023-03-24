58d5614a55359e4b6a4ceebe4fdab18d7cb2fc47
吴俊勇 <1170340749@qq.com>
2023-03-24 23:08:35 +0800

    changeDocs

---
+++ src/index.d.ts
@@ -0,0 +1,13 @@
+
+declare module 'vue-progressive-image' {
+	const content: any;
+	/// 这里的 content 可以根据自己的需要，添加需要的类型，这的话可以让 ts 更好的提示
+	/**
+    type content = {
+        test: string
+    }
+   */
+	export = content;
+}
+
+declare module '*.js';
--- types/vue-progressive-image.ts
+++
@@ -1,4 +0,0 @@
-declare module 'vue-progressive-image' {
-	const content: any;
-	export = content;
-}
